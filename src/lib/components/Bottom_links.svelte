45051db0eb7e152fcd34daa6d8892eec6ec3adb5
Marc Hyeler <birdpump@gmail.com>
2023-07-05 11:19:50 -0700

    initial commit

---
+++ src/lib/components/Bottom_links.svelte
@@ -0,0 +1,57 @@
+<style>
+
+    .links-cont{
+        width: 100vw;
+        display: flex;
+        flex-direction: column;
+        row-gap: 5px;
+        margin-bottom: 5px;
+    }
+
+    .links{
+        width: 100%;
+        display: flex;
+        column-gap: 20px;
+        flex-direction: row;
+        /*align-items: ;*/
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    .text{
+        font-family: "Segoe UI", Arial, sans-serif;
+        font-size: 12px;
+        font-weight: 400;
+        line-height: 16px;
+        color: rgb(115,115,115);
+
+    }
+
+    .link:hover{
+        color: rgba(155, 155, 155);
+    }
+
+    a{
+        text-decoration: none;
+    }
+
+    .bottom{
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+</style>
+
+<div class="links-cont">
+    <div class="links">
+        <a href="t"><div class="link text">Home</div></a>
+        <a href="t"><div class="link text">Terms & Conditions</div></a>
+        <a href="t"><div class="link text">Contact Us</div></a>
+        <a href="t"><div class="link text">Join our Mailing List</div></a>
+    </div>
+    <div class="bottom">
+        <div class="text">© 2004 - Present ItsMySeat.com</div>
+    </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Demo from "$lib/components/Demo.svelte";
+</script>
+<style>
+
+
+</style>
+<Demo/>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<div>
+
+</div>
